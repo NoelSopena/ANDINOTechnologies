3d8fbb4956924eeca5daecb1d51367f0293a3111
NoelSopena <noel.sopena@upr.edu>
2015-05-06 12:23:01 -0400

    Update IniciarSesionhtml.php

--- IniciarSesionhtml.php
+++ IniciarSesionhtml.php
@@ -1,8 +1,8 @@
-<?php
+﻿<?php
+	//Creates a session passed via a POST request
 	session_start();
-	session_unset();
 
-    //Includes the libraries to change the language of the page (english/spanish) and the navigational bar
+	//Includes the libraries to change the language of the page (english/spanish) and the navigational bar
 	include_once 'common.php';
 	include 'library.php';
 ?>
@@ -10,6 +10,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<!-- Title of the page and CSS that have the style code of the page -->
 		<title> Iniciar Sesión </title>
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
@@ -18,67 +19,72 @@
 	</head>
 
 	<body>
-
 		<?php 
-		 /*
-        This function displays the information in the navigation bar. It includes the system's header and the
-        language selection dropdown buttons.
-        */
+			/*
+				This function displays the information in the navigation bar. It includes the system's header and the
+				language selection dropdown buttons.
+			*/
 			navbar($lang['language']);
 		?>
 
 		<div class="container">
-			<h1> 
-			<?php 
-			print "$lang[signIn]"; 
-			?> 
-			</h1>
+			<!-- Header of the page -->
+			<h1 class="col-md-offset-1"> <?php print "$lang[signIn]"; ?> </h1>
+
 			<br></br>
 			<!-- Field to enter the username and the password -->
-			<form class="form-inline col-md-offset-1" id="form-user" action="login.php" method="post">
+			<form class="form-horizontal col-md-offset-1" id="form-user" action="login.php" method="post">
 				<!-- Here is the space for the username of the administrator, secretary or attorney -->
-				<div class="row">
-					<label> <?php 
-					print $lang['userName'];
-					 ?>:
-					</label>
-					<input type="text" class="form-control" id="userName" minlength="8" maxlength="15" name="userName" placeholder="<?php print $lang['userName']; ?>" required>
+				<div class="form-group">
+					<label class="col-sm-2 control-label"> <?php print $lang['userName']; ?>: </label>
+					<div class='col-sm-3'>
+						<div id='name1' class=''>
+							<input type="text" class="form-control col-sm-3" id="userName" minlength="8" maxlength="15" name="userName" placeholder="<?php print $lang['userName']; ?>" required oninput="validateFormName(this, 'name_help', 'name1', 'icon_check1');">
+							<span  id ='icon_check1' class=''></span>
+							<span  id ='name_help' class=''></span>
+						</div>
+					</div>				
 				</div>
 
 				<p></p>
 				<!-- Here is the space for the password of the user -->
-				<div class="row">
-					<label>
-					<?php
-					print $lang['userPassword']; 
-					?>:
-					</label>
-					<input type="password" class="form-control" id="userPassword" minlength="8" maxlength="15"  name="userPassword" placeholder="<?php print $lang['userPassword'];	?>" required>
+				<div class="form-group">
+					<label class="col-sm-2 control-label"> <?php print $lang['userPassword']; ?>: </label>
+					<div class='col-sm-3'>
+						<div id='name2' class=''>
+							<input type="password" class="form-control" id="userPassword" minlength="8" maxlength="15"  name="userPassword" placeholder="<?php print $lang['userPassword'];	?>" required oninput="validateFormPwd(this, 'name2', 'icon_check2');">
+							<span  id ='icon_check2' class=''></span>
+							<span  id ='pwd_help' class=''></span>
+						</div>
+					</div>
 				</div>
 
 				<p></p>
-				<!-- checkbox to remember the username and the password of the user -->
-				<div>
-					<label class="checkbox-inline"> 
-						<input type="checkbox" name="optionsRadiosinline" id="optionsRadios3" value="option1">  <?php echo $lang['remember']; ?>
-					</label> 
-
-					<br></br>
-					<!-- Button to start the session -->
-					<div class="row">
-						<button class="btn btn-primary col-md-offset-1" type="submit" form="form-user" > <?php echo $lang['signup']; ?></button>
-						<a class="btn btn-primary" href="changehtml.php"><?php echo $lang['userChange']; ?></a>
-					</div>
+				<!-- Button to start the session -->
+				<div class="row">
+					<button class="btn btn-primary col-md-offset-2" type="submit" form="form-user" > <?php echo $lang['signup']; ?></button>
+					<a class="btn btn-primary" href="changehtml.php"><?php echo $lang['userChange']; ?></a>
+					<a class="btn btn-primary" href="PasswordReset.php"> Forgot Password </a>
 				</div>
 			</form>
-
 		</div>
+
 		<?php
-		//print_r($lang);
+			//If a user tries to log in with an incorrect username or password this will prompt the user an error.
+			if(isset($_GET['e']) && $_GET['e'] == 'error'){ 
+				print "<script>";
+				print "var helpText = document.getElementById('name_help');";
+				print "helpText.style.color = 'red';"; //Prompt message character color
+				print "helpText.innerHTML = 'Incorrect Username.<br>';";
+				print "var helpText1 = document.getElementById('pwd_help');";
+				print "helpText1.style.color = 'red';";
+				print "helpText1.innerHTML = 'Incorrect Password';";      
+				print "</script>";
+			}
 		?>
 
 		<script src="js/jquery-1.11.2.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
-
+		<script src="js/logInValidation.js"></script>
 	</body>
 </html>
